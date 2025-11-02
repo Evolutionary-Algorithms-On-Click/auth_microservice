04a3256f3e8a0b9ef79199c7b19bcd98e064fc93
Sharath S R <sharathsr@Sharaths-MacBook-Pro.local>
2025-11-02 19:14:03 +0530

    Added CSRF Protection to GRPC endpoints

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ import (
 	"os"
 	"runtime"
 
+	"reflect"
+
 	"aidanwoods.dev/go-paseto"
 	"github.com/rs/cors"
 	"google.golang.org/grpc"
@@ -27,6 +29,8 @@ var (
 
 func serveHTTP() {
 	logger := util.SharedLogger
+
+	fmt.Println("type of logger", reflect.TypeOf(&logger))
 	// Register routes.
 	http.HandleFunc(routes.TEST, controller.Test)
 	http.HandleFunc(routes.REGISTER, controller.Register)
@@ -82,15 +86,15 @@ func serveGRPC() {
 
 func main() {
 
+	logger, err := util.InitLogger(os.Getenv("ENV"))
+	util.SharedLogger = logger
 	HTTP_PORT = fmt.Sprintf(":%v", os.Getenv("HTTP_PORT"))
 	GRPC_PORT = fmt.Sprintf(":%v", os.Getenv("GRPC_PORT"))
 
-	logger, err := util.InitLogger(os.Getenv("ENV"))
 	if err != nil {
 		fmt.Println("failed to init logger:", err)
 		return
 	}
-	util.SharedLogger = logger
 
 	// Initialize db with schema.
 	if err := db.InitDb(context.Background()); err != nil {
